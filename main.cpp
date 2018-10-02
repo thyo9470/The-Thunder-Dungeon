477346ec505d714c50beb009226cd14e53585159
miyo6032 <miyo6032@colorado.edu>
2018-10-02 12:54:28 -0600

    Add QT project demo

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+#include <QJsonDocument>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0, 0, 100, 100);
+
+    scene->addItem(rect);
+
+    QGraphicsView * view = new QGraphicsView(scene);
+
+    view->show();
+
+    return a.exec();
+}
